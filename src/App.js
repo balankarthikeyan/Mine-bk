2f1f0252c25b858f792d264910558dbd00cd6786
Karthikeyan B <bkarthikeyan@skava.com>
2019-05-04 01:29:07 +0530

    COde updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import { ButtonIcon, mycomponent, styled } from "bk-my-material";
-const HeartinIcon = styled(ButtonIcon)`background: black;`;
+const HeartinIcon = styled(ButtonIcon)`
+  background: black;
+`;
 class App extends Component {
   render() {
     const text = "My " + mycomponent.name;
     return (
       <div className="App">
-        <h1>UI Customization</h1>
+        <h1>UI Customization --- ADDED</h1>
         <ButtonIcon icon="settings" align="right" text={"IRONMAN"} />
         <HeartinIcon align="right" text={text} />
       </div>
